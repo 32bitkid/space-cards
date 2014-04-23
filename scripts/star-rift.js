a8e5688297c2dae39cc6b07df32052c91acd354c
James Holmes <32bitkid@gmail.com>
2014-04-23 17:59:07 -0400

    Fluenty interface

--- scripts/star-rift.js
+++ scripts/star-rift.js
@@ -1,78 +1,99 @@
-var defineGame = (function() {
-  
-  var defineCard = function() {
-    
+var defineCard = (function() {
+  return function(title, description) {
+    var requirements = [];
     var consequences = [];
-    var countersCards = [];
     
     var card = {
-      does: function(consequence) { consequences.push(consequence); },
-      counters: function(card) { countersCards.push(card); },
-      counteredBy: function(card) { card.counters(this); }
+      requires: function(fn) { requirements.push(fn); return card; },
+      does: function(fn) { consequences.push(fn); return card; },
+      play: function(source, target) {
+        var requirementChecker = function(requirement) { return requirement.call(card, source, target); };
+        var valid = _.all(requirements,  requirementChecker);
+        if (!valid) return console.log("Could not apply \"" + title +"\"", source, "to", target), false;
+        var consequencePerformer = function(fn) { fn.call(card, source, target); };
+        _.each(consequences, consequencePerformer);
+      },
     };
+    
     return card;
   };
-  
-  var shuffledDeck = (function() {
-    var extensions = {
-      
-    };
-    
-    return function(target) {
-      return _.defaults(target, extensions);
-    };
-  }).call(this);
-  
-  var createDeck = (function() {
-    var cards = [];
-    return {
-      add: function(count, card) {
-        _.times(count, function() { cards.push(card); });
-      },
-      shuffle: function() {
-        return shuffledDeck(_.shuffle(cards));
-      }
-    };
-  });
-  
-  var staticHelpers = {
-    defineCard: defineCard
-  };
-  
-  var fn = function(init) {
-    var gameRules = {
-      deck: createDeck()
-    };
-    _.defaults(gameRules, staticHelpers);
-    init.call(null, gameRules);
-    
-    var gameInProgress = function(players) {
-      this.deck = gameRules.deck.shuffle();
-      this.players = players;
-    };
-    
-    gameInProgress.prototype.deal = function() {
-      console.log(this.deck);
-      console.log("Dealing!");
-    }
-    
-    return gameInProgress;
-  };
-  
-  return fn;
 }).call(this);
 
-var starRift = defineGame(function(game) {
-  var attackCard = game.defineCard();
-  var evadeCard = game.defineCard();
-  
-  attackCard.does(function(source, target) { target.damage(); });
-  evadeCard.counters(attackCard);
-  
-  game.deck.add(1, attackCard);
-  game.deck.add(1, evadeCard);
-});
+var getArg = _.memoize(function(index) { return function() { return arguments[index]; } });
 
-var currentGame = new starRift();
-currentGame.deal();
+var source = getArg(0);
+var target = getArg(1);
 
+var addOf = function(fn) {
+  fn.of = function(who) { return _.compose(fn, who) };
+  return fn;
+}
+
+var mutate = function(property) {
+  
+  var set = function(value) {
+    return addOf(function(player) {
+      player[property] = value;
+    });
+  };
+  
+  var add = function(amount) {
+    return addOf(function(player) {
+      player[property] += amount;
+    });
+  };
+  
+  var remove = function(amount) { return add(-amount); };
+  
+  return {
+    add: add,
+    charge: add,
+    increase: add,
+    remove: remove,
+    drain: remove,
+    decrease: remove,
+    set: set
+  };
+};
+
+var check = (function() {
+  var comps = {
+    eq  : function(a,b) { return a === b; },
+    ne  : function(a,b) { return a !== b; },
+    gt  : function(a,b) { return a >   b; },
+    gte : function(a,b) { return a >=  b; },
+    lt  : function(a,b) { return a <   b; },
+    lte : function(a,b) { return a <=  b; }
+  };
+  
+  return function(property) {
+    var propertyChecks = {};
+    _.each(comps, function(fn, key) {
+      propertyChecks[key] = function(amount) {
+        return addOf(function(player) {
+          return fn(player[property], amount);
+        });
+      };
+    });
+    return propertyChecks;
+  };
+}).call(this);
+
+
+
+var thrusters = defineCard("Thusters", "Move forward one space")
+  .requires(check("battery").gte(1).of(source))
+  .does(mutate("battery").drain(1).of(source));
+  
+var lasers = defineCard("Lasers", "Shoot some lasers!")
+  .requires(check("battery").gte(2).of(source))
+  .does(mutate("battery").drain(2).of(source))
+  .does(mutate("shields").drain(1).of(target))
+  
+var player1 = {battery: 1, shields: 2};
+var player2 = {battery: 5, shields: 1};
+
+thrusters.play(player1);
+lasers.play(player2, player1);
+
+console.log(player1,player2);
